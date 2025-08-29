00acc7b29753cbe340c8db66632aa8bcc62cbcaa
VanDoeun12 <vandoeun.san@gimal.com>
2025-08-29 21:52:23 +0700

    done on nav(simple-react-project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+import DefaultLayout from '../layout/DefaultLayout';
+import Nav from '../nav';
+
+function App() {
+  return (
+    <div >
+      <DefaultLayout>
+        {/* Nav */}
+        <Nav />
+        {/* End of Nav */}
+
+        {/* Hero */}
+        {/* End of Hero */}
+
+        {/* Menu */}
+        {/* End of Menu */}
+
+        {/* Contact */}
+        {/* End of Contact */}
+
+        {/* Footer */}
+        {/* End of Footer */}
+
+      </DefaultLayout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/DefaultLayout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+
+//create use props
+interface DefaultLayoutProps {
+    children?: React.ReactNode;
+}
+
+
+const DefaultLayout: React.FC <DefaultLayoutProps> = (props) => {
+    const { children } = props;
+    return(
+        <div className="container">
+            {children}
+        </div>
+       
+    )
+}
+
+export default DefaultLayout; 
